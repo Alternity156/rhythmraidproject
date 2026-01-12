93b02348e473f137f84166d39d18b2dbd65609dd
Alternity156 <alternity156@gmail.com>
2026-01-12 11:32:45 -0400

    Game now creates the Songs folder

--- Rhythm Raid Project/Assets/Scripts/GameManager.cs
+++ Rhythm Raid Project/Assets/Scripts/GameManager.cs
@@ -1,22 +1,49 @@
 using UnityEngine;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager I;
+
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject editor;
     [SerializeField] private GameObject editorMenu;
 
+    static string songsFolder = "Songs";
+
     void StartState()
     {
+        Debug.Log("Setting up start state");
         mainMenu.SetActive(true);
         editor.SetActive(false);
         editorMenu.SetActive(false);
+    }
+
+    void FolderSetup()
+    {
+        string songsPath = Path.Combine(Application.dataPath, songsFolder);
+
+        if (!Directory.Exists(songsPath))
+        {
+            Directory.CreateDirectory(songsPath);
+            Debug.Log("Created Songs folder");
+        }
+        else
+        {
+            Debug.Log("Songs folder exists");
+        }
+    }
+
+    private void Awake()
+    {
+        I = this;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         StartState();
+        FolderSetup();
     }
 
     // Update is called once per frame
